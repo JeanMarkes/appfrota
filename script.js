14d532efbc6cf0cb48bb371fe8e8e8980c2c7b40
JeanMarkes <131918798+JeanMarkes@users.noreply.github.com>
2023-11-01 11:21:43 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,31 @@
-function exibirTexto() {
-    const consulta = document.getElementById("consultaInput").value;
+function pesquisar() {
+    const consulta = document.getElementById("consultaInput").value.toUpperCase(); // Converte a consulta para maiúsculas
+    const url = "https://docs.google.com/spreadsheets/d/e/2PACX-1vRjVt_E3urPlxsUzznawEgySBt08oBIfuf_qhgFmiSpx5Xu8FN1EJExvVVnfAv_hx4CBbjpNybR6Ibc/pubhtml"; // Substitua pelo URL da sua planilha compartilhada
 
-    if (consulta.trim() === "") {
-        alert("Por favor, digite um texto para pesquisar.");
-    } else {
-        alert("Texto a ser pesquisado: " + consulta);
-    }
+    fetch(url)
+        .then(response => response.text())
+        .then(data => {
+            const linhas = data.split('\n');
+            const resultados = [];
+
+            for (let i = 1; i < linhas.length; i++) { // Começa em 1 para pular o cabeçalho
+                const colunas = linhas[i].split(',');
+                const empresa = colunas[0].trim(); // Coluna "Empresa"
+                const placa = colunas[1].trim().toUpperCase(); // Coluna "Placa"
+
+                if (placa === consulta) {
+                    resultados.push(`Empresa: ${empresa}, Placa: ${placa}`);
+                }
+            }
+
+            const resultado = document.getElementById("resultado");
+            if (resultados.length > 0) {
+                resultado.innerHTML = "Resultado da pesquisa:<br>" + resultados.join("<br>");
+            } else {
+                resultado.innerHTML = "Nenhum resultado encontrado para a placa informada.";
+            }
+        })
+        .catch(error => {
+            console.error("Erro ao consultar a planilha: " + error);
+        });
 }
