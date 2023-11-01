f728af62d89de8190c4953089e5a8ae3df08bdf8
JeanMarkes <131918798+JeanMarkes@users.noreply.github.com>
2023-11-01 11:10:02 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,29 +1,9 @@
-function pesquisar() {
+function exibirTexto() {
     const consulta = document.getElementById("consultaInput").value;
-    const url = "https://docs.google.com/spreadsheets/d/e/2PACX-1vRjVt_E3urPlxsUzznawEgySBt08oBIfuf_qhgFmiSpx5Xu8FN1EJExvVVnfAv_hx4CBbjpNybR6Ibc/pub?output=csv";
 
-    fetch(url)
-        .then(response => response.text())
-        .then(data => {
-            // Analise os dados do arquivo CSV
-            const linhas = data.split('\n');
-            const resultados = [];
-
-            linhas.forEach(linha => {
-                const colunas = linha.split(',');
-                // Supondo que a coluna que você deseja pesquisar seja a primeira (índice 0)
-                const valorColuna = colunas[0];
-
-                if (valorColuna.toLowerCase().includes(consulta.toLowerCase())) {
-                    resultados.push(colunas.join(', '));
-                }
-            });
-
-            // Exiba os resultados na página
-            const resultado = document.getElementById("resultado");
-            resultado.innerHTML = resultados.join("<br>");
-        })
-        .catch(error => {
-            console.error("Erro ao consultar o arquivo CSV: " + error);
-        });
+    if (consulta.trim() === "") {
+        alert("Por favor, digite um texto para pesquisar.");
+    } else {
+        alert("Texto a ser pesquisado: " + consulta);
+    }
 }
